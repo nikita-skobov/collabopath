006a6e9c78367b612088823ee2b6c4b023eb6494
nikita-skobov <nskobov.websites@gmail.com>
2018-12-05 09:46:41 -0600

    adds fetch functionality to current votes component

--- frontend/src/components/CurrentVotes.js
+++ frontend/src/components/CurrentVotes.js
@@ -1,6 +1,30 @@
+/* global fetch */
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import { getVotesEndpoint } from '../dynamicVars'
+
 export default class CurrentVotes extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      list: [],
+      error: false,
+    }
+  }
+
+  fetchList() {
+    // fetches the getVotes data
+    // which returns an array of currently voted
+    // path objects
+    fetch(getVotesEndpoint)
+      .then(res => res.json())
+      .then((list) => {
+        this.setState({ list, error: false })
+      }).catch((err) => {
+        console.log(err)
+        this.setState({ error: true })
+      })
+  }
 }
