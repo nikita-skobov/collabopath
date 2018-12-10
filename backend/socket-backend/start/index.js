034e818f967abe5787a06533644a19b24fc5f244
nikita-skobov <nskobov.websites@gmail.com>
2018-12-10 14:40:55 -0600

    adds private sockets between ec2 instances

--- backend/socket-backend/start/index.js
+++ backend/socket-backend/start/index.js
@@ -2,22 +2,64 @@ const request = require('request')
 const express = require('express')
 const http = require('http')
 const path = require('path')
+const socketioclient = require('socket.io-client')
 const AWS = require('aws-sdk')
+const serverName = require('os').hostname()
+const socketio = require('socket.io')
+const myconfig = require('./myconfig.json')
+const REGION = myconfig.region
+
+const containsBadWords = require('./containsBadWords')
+
+let FunctionName = ''
+
 const autoscale = new AWS.AutoScaling({
-  region: 'us-east-1',
+  region: REGION,
 })
 const ec2 = new AWS.EC2({
-  region: 'us-east-1',
+  region: REGION,
 })
 
-const socketioclient = require('socket.io-client')
+const lambda = new AWS.Lambda({ region: REGION })
 
-const Filter = require('bad-words')
-const filter = new Filter({ emptyList: true })
-const badWords = ['5h1t', '5hit', 'a55', 'anal', 'anus', 'ar5e', 'arrse', 'arse', 'ass', 'ass-fucker', 'asses', 'assfucker', 'assfukka', 'asshole', 'assholes', 'asswhole', 'a_s_s', 'b!tch', 'b00bs', 'b17ch', 'b1tch', 'balls', 'ballsack', 'bastard', 'beastial', 'beastiality', 'bellend', 'bestial', 'bestiality', 'bi+ch', 'biatch', 'bitch', 'bitcher', 'bitchers', 'bitches', 'bitchin', 'bitching', 'bloody', 'blow job', 'blowjob', 'blowjobs', 'boiolas', 'bollock', 'bollok', 'boner', 'boob', 'boobs', 'booobs', 'boooobs', 'booooobs', 'booooooobs', 'breasts', 'buceta', 'bugger', 'bum', 'bunny fucker', 'butt', 'butthole', 'buttmuch', 'buttplug', 'c0ck', 'c0cksucker', 'carpet muncher', 'cawk', 'chink', 'cipa', 'cl1t', 'clit', 'clitoris', 'clits', 'cnut', 'cock', 'cock-sucker', 'cockface', 'cockhead', 'cockmunch', 'cockmuncher', 'cocks', 'cocksuck', 'cocksucked', 'cocksucker', 'cocksucking', 'cocksucks', 'cocksuka', 'cocksukka', 'cok', 'cokmuncher', 'coksucka', 'coon', 'cox', 'cum', 'cummer', 'cumming', 'cums', 'cumshot', 'cunilingus', 'cunillingus', 'cunnilingus', 'cunt', 'cuntlick', 'cuntlicker', 'cuntlicking', 'cunts', 'cyberfuc', 'cyberfuck', 'cyberfucked', 'cyberfucker', 'cyberfuckers', 'cyberfucking', 'd1ck', 'dick', 'dickhead', 'dildo', 'dildos', 'dirsa', 'dlck', 'dog-fucker', 'doggin', 'dogging', 'donkeyribber', 'doosh', 'duche', 'dyke', 'ejaculate', 'ejaculated', 'ejaculates', 'ejaculating', 'ejaculatings', 'ejaculation', 'ejakulate', 'f u c k', 'f u c k e r', 'fag', 'fagging', 'faggitt', 'faggot', 'faggs', 'fagot', 'fagots', 'fags', 'fanny', 'fannyflaps', 'fannyfucker', 'fanyy', 'fatass', 'fcuk', 'fcuker', 'fcuking', 'felching', 'fellate', 'fellatio', 'fingerfuck', 'fingerfucked', 'fingerfucker', 'fingerfuckers', 'fingerfucking', 'fingerfucks', 'fistfuck', 'fistfucked', 'fistfucker', 'fistfuckers', 'fistfucking', 'fistfuckings', 'fistfucks', 'flange', 'fook', 'fooker', 'fuck', 'fucka', 'fucked', 'fucker', 'fuckers', 'fuckhead', 'fuckheads', 'fuckin', 'fucking', 'fuckings', 'fuckingshitmotherfucker', 'fuckme', 'fucks', 'fuckwhit', 'fuckwit', 'fudge packer', 'fudgepacker', 'fuk', 'fuker', 'fukker', 'fukkin', 'fuks', 'fukwhit', 'fukwit', 'fux', 'fux0r', 'f_u_c_k', 'gangbang', 'gangbanged', 'gangbangs', 'gaylord', 'gaysex', 'goatse', 'hardcoresex', 'heshe', 'hoar', 'hoare', 'hoer', 'homo', 'hore', 'horniest', 'horny', 'hotsex', 'jack-off', 'jackoff', 'jap', 'jerk-off', 'jism', 'jiz', 'jizm', 'jizz', 'kawk', 'knob', 'knobead', 'knobed', 'knobend', 'knobhead', 'knobjocky', 'knobjokey', 'kock', 'kondum', 'kondums', 'kum', 'kummer', 'kumming', 'kums', 'kunilingus', 'l3i+ch', 'l3itch', 'labia', 'lust', 'lusting', 'm0f0', 'm0fo', 'm45terbate', 'ma5terb8', 'ma5terbate', 'masochist', 'master-bate', 'masterb8', 'masterbat*', 'masterbat3', 'masterbate', 'masterbation', 'masterbations', 'masturbate', 'mo-fo', 'mof0', 'mofo', 'mothafuck', 'mothafucka', 'mothafuckas', 'mothafuckaz', 'mothafucked', 'mothafucker', 'mothafuckers', 'mothafuckin', 'mothafucking', 'mothafuckings', 'mothafucks', 'mother fucker', 'motherfuck', 'motherfucked', 'motherfucker', 'motherfuckers', 'motherfuckin', 'motherfucking', 'motherfuckings', 'motherfuckka', 'motherfucks', 'muff', 'mutha', 'muthafecker', 'muthafuckker', 'muther', 'mutherfucker', 'n1gga', 'n1gger', 'nazi', 'nigg3r', 'nigg4h', 'nigga', 'niggah', 'niggas', 'niggaz', 'nigger', 'niggers', 'nob', 'nob jokey', 'nobhead', 'nobjocky', 'nobjokey', 'numbnuts', 'nutsack', 'orgasim', 'orgasims', 'orgasm', 'orgasms', 'p0rn', 'pawn', 'pecker', 'penis', 'penisfucker', 'phonesex', 'phuck', 'phuk', 'phuked', 'phuking', 'phukked', 'phukking', 'phuks', 'phuq', 'pigfucker', 'pimpis', 'piss', 'pissed', 'pisser', 'pissers', 'pisses', 'pissflaps', 'pissin', 'pissing', 'pissoff', 'porn', 'porno', 'pornography', 'pornos', 'prick', 'pricks', 'pron', 'pube', 'pusse', 'pussi', 'pussies', 'pussy', 'pussys', 'rectum', 'retard', 'rimjaw', 'rimming', 's hit', 's.o.b.', 'sadist', 'schlong', 'screwing', 'scroat', 'scrote', 'scrotum', 'semen', 'sex', 'sh!+', 'sh!t', 'sh1t', 'shag', 'shagger', 'shaggin', 'shagging', 'shemale', 'shi+', 'shit', 'shitdick', 'shite', 'shited', 'shitey', 'shitfuck', 'shitfull', 'shithead', 'shiting', 'shitings', 'shits', 'shitted', 'shitter', 'shitters', 'shitting', 'shittings', 'shitty', 'skank', 'slut', 'sluts', 'smegma', 'smut', 'snatch', 'son-of-a-bitch', 'spac', 'spunk', 's_h_i_t', 't1tt1e5', 't1tties', 'teets', 'teez', 'testical', 'testicle', 'tit', 'titfuck', 'tits', 'titt', 'tittie5', 'tittiefucker', 'titties', 'tittyfuck', 'tittywank', 'titwank', 'tosser', 'turd', 'tw4t', 'twat', 'twathead', 'twatty', 'twunt', 'twunter', 'v14gra', 'v1gra', 'vagina', 'viagra', 'vulva', 'w00se', 'wang', 'wank', 'wanker', 'wanky', 'whoar', 'whore', 'willies', 'xxx']
-filter.addWords(...badWords)
-const containsBadWords = (text) => {
-  return filter.isProfane(text)
+function invokeAsync(params) {
+  return new Promise((res, rej) => {
+    lambda.invokeAsync(params, (err) => {
+      if (err) return rej(err)
+      return res()
+    })
+  })
+}
+
+function connectToFriend(hostname, pSockets, auth) {
+  const url = `http://${hostname}/private`
+  console.log(`connecting to friend: ${url}`)
+
+  const s = socketioclient.connect(url, { 'force new connection': true, transports: ['websocket', 'xhr-polling'], reconnection: false })
+  s.on('connect', () => {
+    pSockets[hostname] = s
+    s.emit('auth', auth)
+  })
+
+  s.on('disconnect', () => {
+    console.log(`WE DISCONNECTED FROM ${hostname}!`)
+    delete pSockets[hostname]
+
+    const invokeParams = {
+      FunctionName,
+      InvokeArgs: JSON.stringify({}),
+    }
+
+    try {
+      setTimeout(async () => {
+        // wait 2 minutes in case it is still terminating
+        await invokeAsync(invokeParams)
+      }, 120000)
+    } catch (e) {
+      console.log('failed to invoke')
+      console.log(e)
+    }
+  })
 }
 
 function getMetaData(type) {
@@ -66,31 +108,80 @@ function describeAutoScalingInstances(id) {
   })
 }
 
-const serverName = require('os').hostname()
+
 const app = express()
 const server = http.createServer(app)
+const has = Object.prototype.hasOwnProperty
+const pSockets = {}
+const badActorList = []
+FunctionName = myconfig.functionname
 
-const io = require('socket.io')(server, {
+
+const io = socketio(server, {
   transports: ['websocket', 'xhr-polling']
 })
 
-const pSockets = []
-
 const privateio = io.of('/private')
+const publ = io.of('/b')
 
 privateio.on('connection', (socket) => {
   console.log('got friend connection')
-  console.log(socket)
+  const host = socket.handshake.headers['x-real-ip']
+
+  if (badActorList.indexOf(host) !== -1) {
+    socket.disconnect()
+    return null
+  }
+
+  socket.on('msg', (msg) => {
+    const friendIP = socket.handshake.headers['x-real-ip']
+    console.log(`got a message from FRIEND: ${friendIP}`)
+    console.log(msg)
+    if (socket.authYes) {
+      publ.emit(msg.type, msg.body)
+    }
+  })
+
+  socket.on('auth', (msg) => {
+    if (msg === 'abc12345') {
+      socket.authYes = true
+    }
+  })
+
+  setTimeout(() => {
+    if (!socket.authYes) {
+      const socketIP = socket.handshake.headers['x-real-ip']
+      socket.disconnect()
+      badActorList.push(socketIP)
+    }
+  }, 2200)
+
+  try {
+    const spl = host.split('.')
+    const hostname = `ip-${spl[0]}-${spl[1]}-${spl[2]}-${spl[3]}.ec2.internal`
+    if (!has.call(pSockets, hostname)) {
+      // only connect if not already connected
+      console.log('NEW FRIEND')
+      connectToFriend(hostname, pSockets, 'abc12345')
+    }
+  } catch (e) {
+    // do nothing
+  }
 })
 
 
-io.on('connection', (socket) => {
-  console.log('blabadshsda')
-  console.log('got connection pih')
+publ.on('connection', (socket) => {
+  console.log('got public connection')
   socket.on('msgi2', (msg) => {
     socket.broadcast.emit('msgo2', msg)
     socket.emit('msgo2', msg)
+
+    Object.keys(pSockets).forEach((key) => {
+      pSockets[key].emit('msg', { type: 'msgo2', body: msg })
+    })
   })
+
+  socket.emit('servername', serverName)
 })
 
 
@@ -115,7 +206,7 @@ server.listen(3000, () => {
 
 async function main() {
   const myInstanceId = await getMetaData('meta-data/instance-id')
-  const myPrivateIp = await getMetaData('meta-data/local-ipv4')
+  const myPrivateDnsName = await getMetaData('meta-data/local-hostname')
   const instanceData = await describeAutoScalingInstances(myInstanceId)
   const { AutoScalingGroupName } = instanceData.AutoScalingInstances[0]
   const autoScaleData = await describeAutoScalingGroups(AutoScalingGroupName)
@@ -135,21 +226,20 @@ async function main() {
   })
 
   instances2.forEach((item) => {
-    const { PrivateIpAddress } = item
-    if (PrivateIpAddress !== myPrivateIp) {
-      myFriendsIps.push(PrivateIpAddress)
+    const { PrivateDnsName } = item
+    if (PrivateDnsName !== myPrivateDnsName) {
+      myFriendsIps.push(PrivateDnsName)
     }
   })
 
-  console.log(myPrivateIp)
+  console.log(`my private dns: ${myPrivateDnsName}`)
+  console.log('friends ips:')
   console.log(myFriendsIps)
-  console.log('asdsadas')
 
-  myFriendsIps.forEach((ip) => {
-    const url = `http://${ip}`
-    console.log(url)
-    pSockets.push(socketioclient.connect(url, { 'force new connection': true }))
-    console.log(pSockets)
+  myFriendsIps.forEach((hostname) => {
+    if (hostname === '') return null
+    connectToFriend(hostname, pSockets, 'abc12345')
+    return null
   })
 }
 
