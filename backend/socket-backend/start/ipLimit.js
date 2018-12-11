043a770addeb4c063fff389bab44a648dec87890
nikita-skobov <nskobov.websites@gmail.com>
2018-12-10 18:32:25 -0600

    adds functions to track ip actions

---
+++ backend/socket-backend/start/ipLimit.js
@@ -0,0 +1,34 @@
+const has = Object.prototype.hasOwnProperty
+const lastIpAction = {}
+const bannedList = []
+const ipRateLimit = 1000 * 2 // 2 seconds
+
+module.exports.banIP = (ip) => {
+  bannedList.push(ip)
+}
+
+module.exports.recordIpAction = (ip) => {
+  const rightNow = new Date().getTime()
+  lastIpAction[ip] = rightNow
+}
+
+module.exports.ipIsAllowed = (ip) => {
+  if (bannedList.indexOf(ip) !== -1) {
+    // if the ip is in the bannedList they are NOT allowed
+    return false
+  }
+
+  if (!has.call(lastIpAction, ip)) {
+    // if new IP, then yes they are allowed
+    return true
+  }
+
+  const rightNow = new Date().getTime()
+  if (lastIpAction[ip] < rightNow - ipRateLimit) {
+    // enough time has passed, so now they can send another message
+    return true
+  }
+
+  // otherwise return false
+  return false
+}
