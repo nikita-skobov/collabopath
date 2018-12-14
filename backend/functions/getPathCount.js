6627535a24238bf2e18270d1f62598e40aa8ffc5
nikita-skobov <nskobov.websites@gmail.com>
2018-12-14 12:35:27 -0600

    adds basic scan functionality

--- backend/functions/getPathCount.js
+++ backend/functions/getPathCount.js
@@ -1,9 +1,20 @@
 const functions = require('./functions')
 
 module.exports = function getPathCount() {
-  return new Promise((res, rej) => {
+  return new Promise(async (res, rej) => {
     try {
-      return res({ Paths: 10, endPaths: 3 })
+      const scanParams = {
+        TableName: process.env.DYNAMO_TABLE,
+        ExpressionAttributeValues: {
+          ':a': {
+            S: '.', // only return items where votes = .
+          }, // that means the path object has been finalized
+        },
+        FilterExpression: 'votes = :a',
+      }
+      const results = await functions.scanTable(scanParams)
+
+      return res({ Paths: results.Count, endPaths: 3 })
     } catch (e) {
       return rej(e)
     }
