226dba11c17a90942e7c5301a19dcf1d540da844
nikita-skobov <nskobov.websites@gmail.com>
2018-12-14 12:55:13 -0600

    adds get count item before scan for testing

--- backend/functions/getPathCount.js
+++ backend/functions/getPathCount.js
@@ -3,6 +3,21 @@ const functions = require('./functions')
 module.exports = function getPathCount() {
   return new Promise(async (res, rej) => {
     try {
+      const getItemParams = {
+        TableName: process.env.DYNAMO_TABLE,
+        Key: {
+          pathId: {
+            S: 'count',
+          },
+          votes: {
+            S: 'count',
+          },
+        },
+      }
+
+      const pathCount = await functions.getItemSingle(getItemParams)
+      console.log(pathCount)
+
       const scanParams = {
         TableName: process.env.DYNAMO_TABLE,
         ExpressionAttributeValues: {
@@ -14,7 +29,7 @@ module.exports = function getPathCount() {
       }
       const results = await functions.scanTable(scanParams)
 
-      return res({ Paths: results.Count, endPaths: 3 })
+      return res({ Paths: results.Count, pathCount })
     } catch (e) {
       return rej(e)
     }
