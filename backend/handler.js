849d01efc11ce8ef09912747c8c9e01c9e6ca452
nikita-skobov <nskobov.websites@gmail.com>
2018-12-14 12:22:10 -0600

    adds function to handler

--- backend/handler.js
+++ backend/handler.js
@@ -7,6 +7,42 @@ const { getPathObj } = functions
 
 const has = Object.prototype.hasOwnProperty
 
+module.exports.getPathCount = async (event, context) => {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': `https://${process.env.DOMAIN}.com`, // Required for CORS support to work
+    'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+  }
+  let statusCode = 500
+  let body = { error: 'Unable to complete request: addReport' }
+  try {
+    let userIP = event.headers['X-Forwarded-For']
+    if (userIP.includes(',')) {
+      // get the real user ip, not the CDN ip
+      userIP = userIP.substr(0, userIP.indexOf(','))
+    } else if (userIP.length === 0) {
+      // if x-forwarded-for is empty, use the CDN ip as last resort
+      userIP = event.requestContext.identity.sourceIp
+    }
+    // the last case is if the length is NOT 0, then the
+    // x-forwarded-for string is the entire user ip, no need
+    // for stripping commas
+
+    body = {}
+    statusCode = 200
+  } catch (e) {
+    headers = e.headers || headers
+    statusCode = e.statusCode || statusCode
+    body = e.body || body
+  }
+
+  return {
+    statusCode,
+    headers,
+    body: JSON.stringify(body),
+  }
+}
+
 module.exports.report = async (event, context) => {
   let headers = {
     'Content-Type': 'application/json',
